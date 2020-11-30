17968031a9f517fee95aad2e927aa433da7e6623
rayalva407 <rayalva407@gmail.com>
2020-11-29 22:29:42 -0700

    sets up routes

--- do-something-frontend/src/App.js
+++ do-something-frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Home'
 import Nav from './Components/Nav'
 import MyList from './Components/MyList'
 import ActivityDetail from './Components/ActivityDetail'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
@@ -31,9 +32,11 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        <Home activityProp={this.state.activity} clickProp={this.handleClick}/>
-        <MyList />
-        <ActivityDetail />
+        <Router>
+          <Route exact path='/' render={props => <Home {...props} activityProp={this.state.activity} clickProp={this.handleClick}/>} />
+          <Route exact path='/mylist' component={MyList} />
+          <Route exact path='/activity/:id' component={ActivityDetail} />
+        </Router>
       </div>
     );
   }
