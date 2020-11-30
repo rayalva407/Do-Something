9d061cd7dbbf31502b0f5b3d410b64188826680b
rayalva407 <rayalva407@gmail.com>
2020-11-30 00:59:21 -0700

    adds index and create actions in activities controller

--- do-something-backend/app/controllers/activities_controller.rb
+++ do-something-backend/app/controllers/activities_controller.rb
@@ -1,2 +1,22 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    activities = Activity.all
+    render json: activities
+  end
+
+  def create
+    activity = Activity.create(tracker_params)
+    if activity.save?
+      render json: activity, except: [:created_at, :updated_at]
+    else
+      render json: {message: "Error! Could not save!"} 
+  end
+  
+  private
+
+    def activity_params
+      params.require(:activity).permit(:name, :type)
+    end
+
 end
