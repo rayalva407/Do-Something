da10e2e609d87d30b76829af928ac3a3d9fbb2e3
rayalva407 <rayalva407@gmail.com>
2020-11-30 01:40:50 -0700

    removes fetchActivities

--- do-something-frontend/src/Components/MyList.js
+++ do-something-frontend/src/Components/MyList.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-async fetchActivites() {
-  const url = 'http://localhost:3001/activities'
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data)
-}
-
 const MyList = () => {
   return (
     <div>
